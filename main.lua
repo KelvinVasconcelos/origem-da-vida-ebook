a8e0b1e3493fba716f9f3bc464df0e6e12677552
KelvinVasconcelos <kelvinvasconcelos2001@gmail.com>
2023-03-05 17:11:30 -0300

    feat: first commit

---
+++ main.lua
@@ -0,0 +1,5 @@
+display.setStatusBar( display.HiddenStatusBar )
+display.setDefault("background", 9/255, 6/255, 12/255)
+
+local composer = require("composer")
+composer.gotoScene("src.pages.cover");
