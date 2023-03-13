6484537e521473a758a87f4956162331ae141914
KelvinVasconcelos <kelvinvasconcelos2001@gmail.com>
2023-03-13 11:06:46 -0300

    feat: create animation page five

--- main.lua
+++ main.lua
@@ -2,4 +2,4 @@ display.setStatusBar( display.HiddenStatusBar )
 display.setDefault("background", 9/255, 6/255, 12/255)
 
 local composer = require("composer")
-composer.gotoScene("src.pages.pageSeven");
+composer.gotoScene("src.pages.pageFive");
--- src/pages/pageFive.lua
+++ src/pages/pageFive.lua
@@ -1,8 +1,13 @@
 local composer = require( "composer" )
 local scene = composer.newScene()
+local physics = require( "physics" )
+local timer = require( "timer" )
 
 local backButton
 local forwardButton
+local meteorAnimationImage
+local dirtImage 
+local explosionAnimationImage
 
 local function onBackPage( self, event )
 	if event.phase == "ended" or event.phase == "cancelled" then
@@ -20,8 +25,83 @@ local function onNextPage( self, event )
 	end
 end
 
+local function onCollision(event)
+  if event.phase == "began" then
+      if (event.object1 == meteorAnimationImage and event.object2 == dirtImage) or
+          (event.object1 == dirtImage and event.object2 == meteorAnimationImage) then
+          transition.to( meteorAnimationImage, { alpha = 0, time = 300 } )
+          transition.to( redRect, { alpha = 1, time = 2000 } )
+          explosionAnimationImage.alpha = 1
+          timer.performWithDelay( 300, function()
+            explosionAnimationImage:play()
+          end)
+          transition.to( explosion, { alpha = 0, time = 2000 } )
+          timer.performWithDelay( 3000, function()
+            dirtImage.fill = {type = "image", filename = "src/assets/animations/plants-page-five.png"}
+          end)
+      end
+  end
+end
+
+local function onAccelerate( event )
+  if event.isShake then
+    meteorAnimationImage:play()
+    transition.to( meteorAnimationImage, { x = display.contentWidth * 0.5, y = display.contentHeight, time = 2000 } )
+  end
+end
+
+Runtime:addEventListener("collision", onCollision)
+Runtime:addEventListener( "accelerometer", onAccelerate )
+
 function scene:create( event )
 	local sceneGroup = self.view
+
+  physics.start()
+  physics.setGravity( 0, 0 )
+
+  dirtImage = display.newImageRect('src/assets/animations/dirt-page-five.png', display.contentWidth, display.contentHeight * 0.2)
+  dirtImage.x = display.contentWidth * 0.5
+  dirtImage.y = display.contentHeight * 0.9
+  sceneGroup:insert(dirtImage)
+  physics.addBody( dirtImage, "static")
+
+  explosionAnimationImage = graphics.newImageSheet( 'src/assets/animations/explosion-page-five.png', {
+    width = 138.4,
+    height = 180.5,
+    numFrames = 10
+  })
+  explosionAnimationImage = display.newSprite( explosionAnimationImage,  { 
+    name = "normal", 
+    start = 1, 
+    count = 10, 
+    time = 1000, 
+    loopCount = 1 
+  })
+  explosionAnimationImage.x = display.contentWidth * 0.5
+  explosionAnimationImage.y = display.contentHeight * 0.85
+  explosionAnimationImage.alpha = 0
+  explosionAnimationImage:scale(2.5, 2.5)
+  sceneGroup:insert( explosionAnimationImage )
+  physics.addBody( explosionAnimationImage, "static")
+
+  meteorAnimationImage = graphics.newImageSheet( 'src/assets/animations/meteor-page-five.png', {
+    width = 74.4,
+    height = 55.8,
+    numFrames = 40
+  })
+  meteorAnimationImage = display.newSprite( meteorAnimationImage,  { 
+    name = "normal", 
+    start = 1, 
+    count = 40, 
+    time = 4000, 
+    loopCount = 0 
+  })
+  meteorAnimationImage.x = display.contentWidth * -0.1
+  meteorAnimationImage.y = display.contentHeight * -0.1
+  meteorAnimationImage:rotate( 20 )
+  meteorAnimationImage:scale(4, 4)
+  sceneGroup:insert( meteorAnimationImage )
+  physics.addBody( meteorAnimationImage, "dynamic", { isSensor = true } )
 
   local cluePage = display.newImageRect('src/assets/texts/clue-page-five.png', display.contentWidth * 0.8, display.contentWidth * 0.045)
   cluePage.x = display.contentWidth * 0.5
